744722ccd255a19e2b2ca6f76b16fc883e2e88b5
Roma Blunt <rdlastochkin@gmail.com>
2025-01-28 10:50:57 +0400

    первый функционал

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ConferencePage from './';
+import ConferencePage from './pages/Conference/ConferencePage';
 
 const App = () => (
   <Router>
